562c6bcc28f09756b837802ea2267a56f1277dc9
Hector Ramirez <heectorr90@gmail.com>
2024-03-01 20:21:37 -0600

    primer commit

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-counter',
+    template: '<h3>counter : {{counter}} </h3><button (click)="increment(-1)">-1</button><button (click)="reset()">Reset</button><button (click)="increment(1)">+1</button>'
+})
+
+export class CounterComponent{
+    public counter: number = 10;
+
+    increment( value: number): void {
+        this.counter += value;
+    }
+
+    reset(){
+        this.counter = 10;
+    }
+
+}
